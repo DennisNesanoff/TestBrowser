8dbc358a17c20ffca667dd5de947f5fec5517235
Dennis <nndenninn@gmail.com>
2020-04-20 18:00:57 +0300

    Initial Commit

---
+++ TestBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestBrowser
+//
+//  Created by Dennis Nesanoff on 20.04.2020.
+//  Copyright © 2020 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
